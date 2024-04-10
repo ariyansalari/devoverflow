3c939b79e760c642a7b9bbad4f4bd0c13f33669e
arian@inja.app <ariyan4509@gmail.com>
2024-04-10 16:41:15 +0430

    master

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ const colors :Config ={
     500:"#7B8EC8",
     400:"#858EAD"
   },
+  "accent-blue":"#1DA1F2"
 }
 const config: Config = {
   content: [
@@ -32,10 +33,52 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:{
+      center:true,
+      padding:"2rem",
+      screens:{
+        '2xl':'1400px'
+      }
+    }
     extend: {
+colors,
+fontFamily:{
+  inter:['var(--font-inter)'],
+    spaceGrotesk:['var(--font-spaceGrotesk)']
+},
+boxShadow:{
+  'light-100':"0px 12px 20px 0px rgba(184,184,184,0.03),0px 6px 12px 0px rgba(184,184,184,0.02), 0px 2px 4px 0px rgba(184,184,184,0.03)",
+  "light-200":"10px 10px 20px 0px rgba(218,213,213,0.10)",
+  "light-300":"-10px 10px 20px 0px rgba(218,213,213,0.10)",
+  "dark-100":"0px 2px 10px 0px rgba(46,52,56,0.10)",
+  "dark-200":"2px 0px 20px 0px rgba(39,36,36,0.04)"
+},
+backgroundImage:{
+  'auth-dark':"url(/assets/images/auth-dark.png)",
+  'auth-light':'url(/assets/images/auth-light.png)',
 
+},
+screens:{
+  xs:"420px"
+},
+keyframes:{
+  'accordion-down':{
+    from:{height:0},
+    to:{height :"var(--redix-accordion-content-hight)"}
+  },
+  'accordion-up':{
+    from:{height:"var(--redix-accordion-content-hight)"},
+    to:{height : 0}
+  }
+},
+animation:{
+  "accordion-down":"accordion-down 0.2s ease-out",
+  "accordion-up":"accordion-up 0.2s ease-out",
+
+}
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'),
+  require('@tailwindcss/typography')]
 };
 export default config;
