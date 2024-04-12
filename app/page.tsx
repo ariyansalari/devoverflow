a02ff9f2536908c19c764887ba64ac62e637a69d
ariyansalari <ariyan4509@gmail.com>
2024-04-12 09:32:43 -0700

    tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-   
+   <div>
+    <h1 className="h1-bold">
+      Next.js 13 we're coming!
+    </h1>
+    <h1 className="h2-bold">
+      Next.js 13 we're coming!
+    </h1>
+    <h1 className="h3-bold">
+      Next.js 13 we're coming!
+    </h1>
+   </div>
   );
 }
