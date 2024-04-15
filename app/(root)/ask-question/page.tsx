d3bab270351460537d71a287bd0012d260a847c4
arian@inja.app <ariyan4509@gmail.com>
2024-04-15 18:32:27 +0430

    askQuestionComponent

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,14 @@
+import Question from '@/components/forms/Question/Question'
 import React from 'react'
 
 const AskQuestion = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+      <div className='mt-9'>
+      <Question   />
+      </div>
+    </div>
   )
 }
 
