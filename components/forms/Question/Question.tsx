e060f8f88b7e5e756637f760bd55b311bbdd39b8
arian@inja.app <ariyan4509@gmail.com>
2024-04-16 11:23:55 +0430

    askQuestionEditorTiny

--- components/forms/Question/Question.tsx
+++ components/forms/Question/Question.tsx
@@ -15,12 +15,17 @@ import { useForm } from 'react-hook-form'
 import {zodResolver} from "@hookform/resolvers/zod"
 import { QuestionsSchema } from '@/lib'
 import { z } from "zod"
-
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Badge } from '@/components/ui/badge'
+import Image from 'next/image'
 
 
 
 
 const Question = () => {
+  const editorRef = useRef(null);
+
     const form = useForm<z.infer<typeof QuestionsSchema>>({
         resolver: zodResolver(QuestionsSchema),
         defaultValues: {
@@ -35,6 +40,36 @@ const Question = () => {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
+      }
+      const handleInputKeyDown =(e:React.KeyboardEvent<HTMLInputElement>,field:any)=>{
+        if(e.key==='Enter' && field.name==="tags"){
+          e.preventDefault()
+          const tagInput =e.target as HTMLInputElement
+          const tagValue =tagInput.value.trim()
+          console.log(tagValue);
+          
+          if( tagValue!==''){
+            if(tagValue.length>15){
+              return form.setError ('tags',{
+                type:"required",
+                message :"Tag must be less than 15 characters"
+              }
+            )
+            }
+            if(!field.value.includes(tagValue as never)){
+              form.setValue('tags',[...field.value,tagValue]);
+              tagInput.value=''
+              form.clearErrors('tags')
+            }
+          }else {
+            form.trigger()
+          }
+        }
+      }
+      const handleTagRemove=(tag:string,field:any)=>{
+        const newTags= field.value.filter((t:string)=>t!==tag)
+        form.setValue('tags',newTags)
+
       }
   return (
     
@@ -64,7 +99,28 @@ const Question = () => {
             <FormItem className='flex  w-full  flex-col  gap-3'>
               <FormLabel className='paragraph-semibold text-dark400_light800 '>Detailed explanation of your problem  <span className='text-primary-500'>*</span></FormLabel>
               <FormControl className='mt-3.5'>
-                {/* /TODO : add an editor component */}
+              <Editor
+              
+        apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+        onInit={(_evt, editor) => {
+          // @ts-ignore
+          editorRef.current = editor}}
+        initialValue=""
+        init={{
+          height:350,
+          menubar: false,
+          plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'codesample', 'help', 'wordcount'
+          ],
+          toolbar: 'undo redo | codesample | ' +
+            'bold italic forecolor | alignleft aligncenter |' +
+            'alignright alignjustify | bullist numlist   | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Inter;font-size:16px }'
+        }}
+      />
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
               introduce the problem and expand on what you put in the title. Minimum 20 characters.
@@ -81,7 +137,30 @@ const Question = () => {
             <FormItem className='flex  w-full  flex-col '>
               <FormLabel className='paragraph-semibold text-dark400_light800 '>Tags <span className='text-primary-500'>*</span></FormLabel>
               <FormControl className='mt-3.5'>
-                <Input  className='paragragph-regular no-focus background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border' placeholder="Add tags..." {...field} />
+                <>
+                <Input onKeyDown={(e)=>handleInputKeyDown(e,field)}  className='paragragph-regular no-focus background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border' placeholder="Add tags..." />
+
+                {field.value.length>0 && (
+                <div className='flex-start mt-2.5 gap-2.5'>
+
+                    {field.value.map((tag:any)=>(
+                      <Badge key={tag} className='subtle-medium text-light-400 background-light800_dark300 flex items-center justify-center gap-2 rounded-lg border-none px-4 py-2 capitalize' onClick={()=>handleTagRemove(tag,field)}>
+                        {tag}
+                        <Image 
+                        alt='Close icon'
+                        width={12}
+                        height={12}
+                        src={'/assets/icons/close.svg'}
+                        className='cursor-pointer object-contain invert-0 dark:invert'
+                        />
+                      </Badge>
+                    ))}
+                </div>
+
+                )}
+
+                </>
+
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
                 Be specific and imagine you&apos;re asking a question  to another person.
