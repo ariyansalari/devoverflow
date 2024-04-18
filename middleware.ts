cecb159c3e2dfb6bef236f0e913a837c4b5e38b2
ariyansalari <ariyan4509@gmail.com>
2024-04-18 15:12:57 -0700

    api webhook

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes:[
-        '/',
-        '/api/webhook',
-        '/question/:id',
-        '/tags/',
-        '/tags/:id',
-        '/profile/:id',
-        '/community',
-        '/jobs'
-    ],
-    ignoredRoutes:[
-      '/api/webhook','/api/chatgpt'
-    ]
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "question/:id",
+    "tags",
+    "/tags/:id",
+    "/profile/:id",
+    '/community',
+    '/jobs'
+  ],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
