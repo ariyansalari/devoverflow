15364601b5913ebad27a2076a53108a731b73706
ariyansalari <ariyan4509@gmail.com>
2024-04-18 12:59:50 -0700

    fixingbuild

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     experimental:{
         serverActions:true,
@@ -8,3 +9,13 @@ const nextConfig = {
 };
 
 export default nextConfig;
+module.exports = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        "mongodb-client-encryption": false ,
+        "aws4": false
+      };
+  
+      return config;
+    }
+}
