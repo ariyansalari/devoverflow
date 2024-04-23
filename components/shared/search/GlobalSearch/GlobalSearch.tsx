0974e85e5637af435b7088d3083b322ee60d7886
arian@inja.app <ariyan4509@gmail.com>
2024-04-23 13:47:03 +0430

    local search

--- components/shared/search/GlobalSearch/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch/GlobalSearch.tsx
@@ -1,8 +1,38 @@
+'use client'
 import { Input } from '@/components/ui/input'
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
  const GlobalSearch = () => {
+  const  router =useRouter()
+  const pathname=usePathname()
+  const searchParams=useSearchParams()
+  const query=searchParams.get('q')
+  const [search,setSearch]=useState(query||'')
+  useEffect(()=>{
+const delayDebounceFn=setTimeout(()=>{
+if(search){
+  const newUrl=formUrlQuery({
+    params:searchParams.toString(),
+    key:"q",
+    value:search
+  })
+  router.push(newUrl,{scroll:false})
+}else {
+  console.log(route,pathname);
+  if(pathname===route){
+    const newUrl=removeKeysFromQuery({
+      params:searchParams.toString(),
+      keyToremove:['q']
+    })
+    router.push(newUrl,{scroll:false})
+  }
+  
+}
+},300)
+  },[search,pathname,router,searchParams,query])
   return (
     <div className='relative w-full max-w-[600px] max-lg:hidden '>
         <div className='background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4'>
